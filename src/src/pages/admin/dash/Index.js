10040f23d99c9bc8721d36de7db7b5df088d5f4f
Sagar Patel <76509231+Sagar13Pro@users.noreply.github.com>
2022-12-04 23:40:06 -0500

    init 2

--- src/src/pages/admin/dash/Index.js
+++ src/src/pages/admin/dash/Index.js
@@ -1,10 +1,6 @@
-import React, { useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React from 'react'
 import { Card, CardBody, ContentBody, ContentContainer } from "./admin.css"
 import Layout from "./Admin.layout"
-import { Input, TextArea } from '../../user/login.css'
-import Button from '../../../components/Button/Button'
-import Api from '../../../apis/Api'
 
 function Admin(props) {
 
